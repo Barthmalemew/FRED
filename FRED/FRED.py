97515117f58886fa9f0b8b26202d3529762fc61a
Barthmalemew <kevinrouse105@gmail.com>
2024-04-01 04:32:48 -0400

    Update

--- FRED/FRED.py
+++ FRED/FRED.py
@@ -5,20 +5,15 @@ model = DeepFace.build_model("Emotion")
 
 emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
-# Load face cascade classifier
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Start capturing video
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Convert frame to grayscale
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the frame
     faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     for (x, y, w, h) in faces:
